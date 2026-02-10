cd6dc11349229bcf488c80df455f28e529bbf4e3
Victor <vonvicky123@gmail.com>
2026-02-10 20:56:43 +0300

    chore(debug): Add imports back to signup.ts

--- api/auth/signup.ts
+++ api/auth/signup.ts
@@ -1,5 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { Client } from 'pg';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    return res.status(200).json({ message: 'Signup super minimal OK' });
+    return res.status(200).json({ message: 'Signup imports OK' });
 }
