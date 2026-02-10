0789c99f23c5ba39c841e9b2ab3d2fe1f24efdf9
Victor <vonvicky123@gmail.com>
2026-02-10 20:54:59 +0300

    chore(debug): Overwrite signup.ts with super minimal version

--- api/auth/signup.ts
+++ api/auth/signup.ts
@@ -1,20 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { Client } from 'pg';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // Check if imports work by just logging something related to them?
-    const client = new Client({ connectionString: 'postgres://dummy' }); // Just accessing constructor
-    const hash = await bcrypt.hash('test', 1); // Fast hash
-    const token = jwt.sign({ test: true }, 'secret');
-
-    return res.status(200).json({
-        message: 'Signup minimal debug OK',
-        checks: {
-            client: !!client,
-            hash: !!hash,
-            token: !!token
-        }
-    });
+    return res.status(200).json({ message: 'Signup super minimal OK' });
 }
