e7f3f5ef30240396c679dfcbd6a86916caa13f0c
Victor <vonvicky123@gmail.com>
2026-02-10 21:34:41 +0300

    chore(debug): Remove imports from signup.ts to test bundle overhead

--- api/auth/signup.ts
+++ api/auth/signup.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcryptjs';
+// import jwt from 'jsonwebtoken';
 import { Client } from 'pg';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
