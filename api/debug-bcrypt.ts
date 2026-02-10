cc9c527d191a18d081a97cd823f670215d5f4b6a
Victor <vonvicky123@gmail.com>
2026-02-10 21:19:15 +0300

    chore(debug): Add imports to debug-bcrypt to test bundle size

--- api/debug-bcrypt.ts
+++ api/debug-bcrypt.ts
@@ -1,5 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { Client } from 'pg';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
